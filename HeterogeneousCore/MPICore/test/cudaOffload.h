393c21b47820b20e2d1173c91e2009270437da52
Eric Cano <eric.cano@cern.ch>
2021-11-01 15:10:49 +0100

    Re-merged MPI + CUDA example into a single file.
    Create a scram profile.

--- HeterogeneousCore/MPICore/test/cudaOffload.h
+++
@@ -1,8 +0,0 @@
-#ifndef HeterogeneousCore_MPICode_cudaOffload_h
-#define HeterogeneousCore_MPICode_cudaOffload_h
-
-#include <vector>
-
-void cudaVectorAdd(std::vector<float>& vectorWorkers1, const std::vector<float>& vectorWorkers2);
-
-#endif
